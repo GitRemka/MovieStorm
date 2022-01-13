b4225ab2f004abd81a963c1bf7ab7633e8ac34bf
Romshad <romshad14@mail.ru>
2022-01-13 12:37:35 +0700

    project MovieStorm

--- cinema_site/settings.py
+++ cinema_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(1pysz4!g^_o0_&^#uvtcbmf8=v6vsw#%0co@5h!pnwnu%^uhu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
